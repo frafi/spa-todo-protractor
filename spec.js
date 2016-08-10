934abc6c7e36e7238c70d58294eafc090cd3a296
Fawad Rafi <fawad.rafi@sungard.com>
2016-08-10 14:32:15 -0400

    first commit

---
+++ spec.js
@@ -0,0 +1,62 @@
+describe('Todo', function() {
+
+  it('Test for creating simple todo list', function() {
+
+  });
+  
+  it('Test whether it is possible to create a todo without entering any text', function() {
+
+  });
+
+  it('Test whether it is possible to create a todo list with quotes', function() {
+  });
+    
+  it('Test the placeholder text of the create text area', function() {
+
+  }); 
+  
+  it('Test whether it is not possible to add malicious script contents , it must no display any alert.', function() {
+
+  }); 
+
+  it('Test whether it is possibe to multiple Todo items.', function() {
+
+  });
+  
+  it('Test clearCompleted functionality.', function() {
+
+  }); 
+  
+  it('Test whether active tab displays only the non complete Todo lists.', function() {
+
+  }); 
+  
+  it('Test whether the completed tab displays only the completed Todo lists.', function() {
+
+  });
+    
+  it('Test the count of  completedTodo list is updated properly.', function() {
+
+  });
+    
+  it('Test the functionality of markAllTodo', function() {
+
+  });
+  
+  it('Test the editing functionality on double click.', function() {
+
+  }); 
+  
+  it('Test whether the leading and trailing whitespaces are trimmed while creating todoList', function() {
+
+  });
+  
+  it('Test whether the toggle action of MarkallTodo ', function() {
+
+  });
+  
+  it('Test the title of the page ',function(){
+
+  });
+
+});
